89ddaba4b8254abea3fc8e1b98e59f87cf01a0ad
MrZatto <konradpowrozek31@gmail.com>
2024-01-28 15:08:10 +0100

    Pierwsza wersja programu

--- src/Shop01.java
+++ src/Shop01.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -12,11 +10,7 @@ public class Shop01 extends JFrame {
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JButton logujButton;
-
-    private JButton regButton;
-    private JButton detalicButton;
-    private JTextField mAmount;
-    private JButton closeButton;
+    private JButton zalogujJakoKlientDetalicznyButton;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
@@ -26,10 +20,10 @@ public class Shop01 extends JFrame {
     }
 
     public Shop01() {
-        super("Panel logowania");
+        super("Login panel");
         this.setContentPane(this.panel1);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 600);
+        this.setSize(300, 500);
 
         logujButton.addActionListener(new ActionListener() {
             @Override
@@ -45,57 +39,13 @@ public class Shop01 extends JFrame {
                     JOptionPane.showMessageDialog(Shop01.this, message);
                     Produkty produkty = new Produkty(enteredUsername, isRegularCustomer);
                     produkty.setVisible(true);
-                    Shop01.this.dispose();
+                    Shop01.this.dispose();  // Zamknij okno logowania po zalogowaniu
                 } else {
                     JOptionPane.showMessageDialog(Shop01.this, "Błędne dane logowania!");
                 }
             }
         });
-
-        detalicButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String enteredUsername = "Klient detaliczny";
-                String amountText = mAmount.getText();
-
-                try {
-                    double amount = Double.parseDouble(amountText);
-                    if (amount <= 0) {
-                        JOptionPane.showMessageDialog(Shop01.this, "Podaj poprawną kwotę pieniędzy.");
-                        return;
-                    }
-
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter("detalicClient.txt"))) {
-                        writer.write(amountText);
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-
-                    Produkty produkty = new Produkty(enteredUsername, false);
-                    produkty.setVisible(true);
-                    Shop01.this.dispose();
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(Shop01.this, "Podaj poprawną kwotę pieniędzy.");
-                }
-            }
-        });
-        regButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                RegisterClient registerClient = new RegisterClient();
-                registerClient.setVisible(true);
-                Shop01.this.dispose(); // Zamknij okno logowania
-            }
-        });
-        closeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Shop01.this.dispose();
-            }
-        });
     }
-
-
 
     private boolean checkLoginCredentials(String username, String password) {
         try (BufferedReader reader = new BufferedReader(new FileReader("loginy.txt"))) {
@@ -131,6 +81,6 @@ public class Shop01 extends JFrame {
         } catch (IOException | NumberFormatException ex) {
             ex.printStackTrace();
         }
-        return -1;
+        return -1;  // Wartość domyślna w przypadku błędu
     }
 }
